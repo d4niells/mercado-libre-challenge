c1333082ac7bbb2eaa5a69f89637b7649d773927
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-11-13 13:40:35 -0300

    feat: create ServerSetup class to config express

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import express, { Application } from 'express';
+
+export class SetupServer {
+  private app: Application;
+
+  constructor(private port: number = 5000) {
+    this.app = express();
+  }
+
+  public init(): void {
+    this.setupExpress();
+  }
+
+  public start(): void {
+    this.app.listen(this.port, () => {
+      console.info('Server listening of port: ' + this.port);
+    });
+  }
+
+  private setupExpress(): void {
+    this.app.use(express.json());
+  }
+}
