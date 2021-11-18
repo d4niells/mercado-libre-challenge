86f99942b648b481416a993374c4a7082c4a0c21
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-11-18 20:43:53 -0300

    feat: create app theme

---
+++ app/src/styles/theme/index.ts
@@ -0,0 +1,35 @@
+export default {
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem'
+  },
+  border: {
+    radius: '0.25rem'
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    bold: 600,
+    sizes: {
+      xsmall: '0.875rem',
+      small: '1.8rem',
+      medium: '1rem',
+      large: '1.125rem',
+      xlarge: '1.5rem',
+      xxlarge: '1.75rem',
+      xxxlarge: '2.875rem'
+    }
+  },
+  colors: {
+    primary: '#FFE600',
+    mainBg: '#EEEEEE',
+    black: '#333333',
+    grey: '#999999',
+    darkGrey: '#666666',
+    lightGrey: '#EEEEEE'
+  },
+  spacings: {
+    medium: '1rem',
+    large: '2rem'
+  }
+};
