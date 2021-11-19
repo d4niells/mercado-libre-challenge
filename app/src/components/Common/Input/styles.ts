3718ba43ee590b83b05be36be2bc1f4a14eca90e
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-11-19 18:44:16 -0300

    chore(input): add default placeholder color to InputBase

--- app/src/components/Common/Input/styles.ts
+++ app/src/components/Common/Input/styles.ts
@@ -1,3 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Input = styled.input``;
+export const Input = styled.input`
+  ${({ theme }) => css`
+    color: ${theme.colors.black};
+
+    &::placeholder {
+      color: ${theme.colors.grey};
+    }
+  `}
+`;
