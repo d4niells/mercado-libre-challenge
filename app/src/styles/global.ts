032e181c43ca721fab331254faf3b9f07bef7c1f
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-11-19 18:31:49 -0300

    chore: add base styles to tags a and button

--- app/src/styles/global.ts
+++ app/src/styles/global.ts
@@ -7,16 +7,29 @@ const GloablStyles = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  html {
-    font-size: 62.5%;
-  }
-
   html, body, #__next {
     height: 100%;
   }
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+    background: none;
+    font-weight: 700;
+    cursor: pointer;
+    border: 0;
+    transition: 180ms ease-in-out;
+  }
+
+  button {
+    cursor: pointer;
+    border: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
