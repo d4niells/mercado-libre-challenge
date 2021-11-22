a42e68d5a3b82661c9b5f935bc6012b863fa4fc6
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-11-22 10:23:06 -0300

    feat: create page itemsand list all result from the search

---
+++ app/src/pages/items/index.tsx
@@ -0,0 +1,45 @@
+import { MercadoLivre } from 'api/core';
+import { SearchResponse } from 'api/core';
+
+import Main from 'components/Main';
+import ProductCard from 'components/ProductCard';
+
+import DefaultLayout from 'layouts/default';
+
+type Context = {
+  query: {
+    search: string;
+  };
+};
+
+type Props = {
+  data: SearchResponse[];
+};
+
+const Products = ({ data }: Props) => {
+  return (
+    <DefaultLayout>
+      <Main>
+        <ol>
+          {data.map(({ author, items }) => {
+            return items.map((item) => (
+              <ProductCard key={author.name} product={item} />
+            ));
+          })}
+        </ol>
+      </Main>
+    </DefaultLayout>
+  );
+};
+
+const getServerSideProps = async ({ query }: Context) => {
+  const data = await MercadoLivre.searchProducts(query.search);
+  return {
+    props: {
+      data: data
+    }
+  };
+};
+
+export { getServerSideProps };
+export default Products;
